b4964350a4a5e968bdb51da31885f0fc94a59e9c
Anh Tran Tuan <yorkittran@gmail.com>
2022-10-11 01:10:51 +0700

    feat: add type to folder and system file type

--- app/graphql/types/object/folder_type.rb
+++ app/graphql/types/object/folder_type.rb
@@ -2,8 +2,14 @@ module Types
   module Object
     class FolderType < BaseObject
       field :id, ID
+      field :type, String
       field :name, String
+      field :size, Int
       field :created_at, String
+
+      def type
+        object.class
+      end
 
       def created_at
         object.created_at.to_s
--- app/graphql/types/object/system_file_type.rb
+++ app/graphql/types/object/system_file_type.rb
@@ -2,9 +2,15 @@ module Types
   module Object
     class SystemFileType < BaseObject
       field :id, ID
+      field :type, String
       field :name, String
       field :data, String
+      field :size, Int
       field :created_at, String
+
+      def type
+        object.class
+      end
 
       def created_at
         object.created_at.to_s
