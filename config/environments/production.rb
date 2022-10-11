6ca9c5866f929968bdaae3e502f53dba2121d091
Anh Tran Tuan <yorkittran@gmail.com>
2022-10-11 19:03:16 +0700

    test: host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,5 +91,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.hosts.clear
+  config.hosts << 'api.virtual-file-system.tthtsvyd.com'
 end
