c980b3810a90da7a9562a43fda1bb8b32894fc75
Anh Tran Tuan <yorkittran@gmail.com>
2022-10-11 20:08:08 +0700

    mod: change argument require

--- app/graphql/resolvers/items_resolver.rb
+++ app/graphql/resolvers/items_resolver.rb
@@ -2,7 +2,7 @@ module Resolvers
   class ItemsResolver < BaseResolver
     type Types::Object::ItemsType, null: false
 
-    argument :folder_id, ID, required: false
+    argument :folder_id, ID, required: true
 
     def resolve(args)
       authorize_user!
